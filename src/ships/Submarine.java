10043715169604ebd7606ebc1435e3e5779d65a7
yolonaai <naailafurruq@gmail.com>
2024-11-18 12:32:28 -0600

    Delete src directory

--- src/ships/Submarine.java
+++
@@ -1,7 +0,0 @@
-package ships;
-
-public class Submarine extends Ship {
-    public Submarine(int startX, int startY, boolean horizontal) {
-        super("Submarine", 4, startX, startY, horizontal);
-    }
-}
