f841b6bca834e7f6f84df7c817545c177968ea00
yolonaai <naailafurruq@gmail.com>
2024-11-20 11:37:55 -0600

    Delete src directory

--- src/ships/PatrolBoat.java
+++
@@ -1,7 +0,0 @@
-package ships;
-
-public class PatrolBoat extends Ship {
-    public PatrolBoat(int startX, int startY, boolean horizontal) {
-        super("PatrolBoat", 4, startX, startY, horizontal);
-    }
-}
