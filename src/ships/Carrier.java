4da410d0a3089d0175eb6474c58ecd009f9c0d07
yolonaai <naailafurruq@gmail.com>
2024-11-26 10:37:07 -0600

    Delete src directory

--- src/ships/Carrier.java
+++
@@ -1,7 +0,0 @@
-package ships;
-
-public class Carrier extends Ship {
-    public Carrier(int startX, int startY, boolean horizontal) {
-        super("Carrier", 5, startX, startY, horizontal);
-    }
-}
--- src/ships/PatrolBoat.java
+++
@@ -1,7 +0,0 @@
-package ships;
-
-public class PatrolBoat extends Ship {
-    public PatrolBoat(int startX, int startY, boolean horizontal) {
-        super("PatrolBoat", 2, startX, startY, horizontal);
-    }
-}
